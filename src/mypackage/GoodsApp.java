9d6bbc970718a51ee613b495cb5e500c02d4acfc
sunyoung <bit-user1@bit-user>
2016-09-12 16:01:14 +0900

    0912

---
+++ src/mypackage/GoodsApp.java
@@ -0,0 +1,26 @@
+package mypackage;
+
+import chapter02.Goods;
+
+public class GoodsApp {
+
+	public static void main(String[] args) {
+		 Goods goods = new Goods ();
+		 goods.setName( "nikon");
+		 //goods.price = -1; 로 입력할 가능성이 있기때문에
+		 goods.setPrice (200000);
+		 goods.setCountStock(50);
+		 goods.setCountSold(10);
+		 //같은 패키지 안에서는 적용되지만 다른 패키지는 지정해줘야 한다. 
+		 
+		// System.out.println(goods.name + " : " + goods.price + " : " + 
+		// 						goods.countStock + " : "+goods.countSold);
+		//get으로 바꿔줘야 위가 실행될수 있음.  
+		//외부에서 직접 셋팅 못하게 막는다. 
+		 
+		System.out.println(goods.getName() + " : " + goods.getPrice() + " : " + 
+							goods.getCountStock() + " : "+goods.getCountSold());
+		
+	}
+
+}
